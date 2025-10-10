1613158f627dff8b57f7f0071b1a10a9573d056e
Ricardo <8240227@estg.ipp.pt>
2025-10-10 14:46:44 +0100

    Stacks

---
+++ Stacks/Interface/StackADT.java
@@ -0,0 +1,9 @@
+package Exercicio_1;
+
+public interface StackADT <T> {
+    public void push(T element);
+    public T pop();
+    public T peek();
+    public boolean isEmpty();
+    public int size();
+}
